3775c2ae463827f1bb44fd9dadf8b50becb770b8
Farrukh Karimov <fkarimov4@gmail.com>
2022-04-06 18:21:06 -0400

    added success message after correct verification code submission

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
 
 function App() {
-  const [tel, setTel] = useState('');
+  const [tel, setTel] = useState("");
   const [codeSent, setCodeSent] = useState(false);
-  const [code, setCode] = useState('');
+  const [code, setCode] = useState("");
+  const [codeStatus, setCodeStatus] = useState("");
 
   const handleSendCode = (e) => {
     e.preventDefault();
@@ -28,49 +29,55 @@ function App() {
       body: JSON.stringify({ userNumber: tel, userCode: code }),
     })
       .then((res) => res.json())
+      .then((res) => setCodeStatus(res.status))
       .catch((err) => console.error(err));
   };
 
   return (
-    <section className="flex flex-col items-center py-4">
+    <section className="flex flex-col items-center py-4 bg-slate-100 h-screen">
       {!codeSent ? (
         <form className="w-96">
-        <label className="flex flex-col text-sm font-medium text-slate-700">
-          Phone Number
-          <input
-            type="tel"
-            placeholder="Enter your mobile number"
-            className="p-2 rounded-lg mb-4"
-            value={tel}
-            onChange={(e) => setTel(e.target.value)}
-          />
-        </label>
-        <button
-          className="w-full bg-red-600 text-white py-2 rounded-lg"
-          onClick={handleSendCode}
-        >
-          Submit
-        </button>
-      </form>
-      ): (
+          <label className="flex flex-col text-sm font-medium text-slate-700">
+            Phone Number
+            <input
+              type="tel"
+              placeholder="Enter your mobile number"
+              className="p-2 rounded-lg mb-4"
+              value={tel}
+              onChange={(e) => setTel(e.target.value)}
+            />
+          </label>
+          <button
+            className="w-full bg-red-600 text-white py-2 rounded-lg"
+            onClick={handleSendCode}
+          >
+            Submit
+          </button>
+        </form>
+      ) : (
         <form className="w-96">
-        <label className="flex flex-col text-sm font-medium text-slate-700">
-          Verification Code
-          <input
-            type="tel"
-            placeholder="Enter the code sent to your mobile number"
-            className="p-2 rounded-lg mb-4"
-            value={code}
-            onChange={(e) => setCode(e.target.value)}
-          />
-        </label>
-        <button
-          className="w-full bg-red-600 text-white py-2 rounded-lg"
-          onClick={handleVerifyCode}
-        >
-          Submit
-        </button>
-      </form>
+          <label className="flex flex-col text-sm font-medium text-slate-700">
+            Verification Code
+            <input
+              type="tel"
+              placeholder="Enter the code sent to your mobile number"
+              className="p-2 rounded-lg mb-4"
+              value={code}
+              onChange={(e) => setCode(e.target.value)}
+            />
+          </label>
+          <button
+            className="w-full bg-red-600 text-white py-2 rounded-lg"
+            onClick={handleVerifyCode}
+          >
+            Verify
+          </button>
+          {codeStatus === "approved" && (
+            <p className="text-green-600 text-center m-2">
+              Code verified successfully
+            </p>
+          )}
+        </form>
       )}
     </section>
   );
