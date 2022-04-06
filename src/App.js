143f0d4859ad5f26641969e7da66cb63198b51da
Farrukh Karimov <fkarimov4@gmail.com>
2022-04-06 17:52:36 -0400

    added ability to request 2FA and verify

--- src/App.js
+++ src/App.js
@@ -1,7 +1,77 @@
+import { useState } from "react";
+
 function App() {
+  const [tel, setTel] = useState('');
+  const [codeSent, setCodeSent] = useState(false);
+  const [code, setCode] = useState('');
+
+  const handleSendCode = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3000/2fa", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userNumber: tel }),
+    })
+      .then((res) => res.json())
+      .then(setCodeSent(true))
+      .catch((err) => console.error(err));
+  };
+  const handleVerifyCode = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3000/2fa/verify", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userNumber: tel, userCode: code }),
+    })
+      .then((res) => res.json())
+      .catch((err) => console.error(err));
+  };
+
   return (
-    <section>
-      
+    <section className="flex flex-col items-center py-4">
+      {!codeSent ? (
+        <form className="w-96">
+        <label className="flex flex-col text-sm font-medium text-slate-700">
+          Phone Number
+          <input
+            type="tel"
+            placeholder="Enter your mobile number"
+            className="p-2 rounded-lg mb-4"
+            value={tel}
+            onChange={(e) => setTel(e.target.value)}
+          />
+        </label>
+        <button
+          className="w-full bg-red-600 text-white py-2 rounded-lg"
+          onClick={handleSendCode}
+        >
+          Submit
+        </button>
+      </form>
+      ): (
+        <form className="w-96">
+        <label className="flex flex-col text-sm font-medium text-slate-700">
+          Verification Code
+          <input
+            type="tel"
+            placeholder="Enter the code sent to your mobile number"
+            className="p-2 rounded-lg mb-4"
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
+          />
+        </label>
+        <button
+          className="w-full bg-red-600 text-white py-2 rounded-lg"
+          onClick={handleVerifyCode}
+        >
+          Submit
+        </button>
+      </form>
+      )}
     </section>
   );
 }
